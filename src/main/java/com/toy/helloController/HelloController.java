d63b46710c5d0f63f4c8ef8a4b86f78baee1a3ee
이한빈 <leehan0617@naver.com>
2017-01-17 00:16:45 +0900

    spring 기본 설정 추가

--- src/main/java/com/toy/helloController/HelloController.java
+++ src/main/java/com/toy/helloController/HelloController.java
@@ -1,7 +1,23 @@
 package com.toy.helloController;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
 public class HelloController {
-	public static void main(String[] args) {
-		System.out.println("test");
+	@RequestMapping("/")
+	public String initPage(){
+		System.out.println("test2");
+		return "index";
 	}
+	
+	@RequestMapping("/hello")
+	public String hello(@RequestParam(value="name", required=false, defaultValue="World") String name , Model model){
+		System.out.println("test");
+		model.addAttribute("name" , name);
+		return "helloWorld";
+	}
+	
 }
