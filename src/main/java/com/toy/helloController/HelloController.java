591328ad6e12e2606d5772530400378fa48dbf33
Lee Han Bin <leehan0617@naver.com>
2017-03-08 14:20:34 +0900

    logout 구현

--- src/main/java/com/toy/helloController/HelloController.java
+++ src/main/java/com/toy/helloController/HelloController.java
@@ -3,9 +3,13 @@ package com.toy.helloController;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
@@ -37,6 +41,16 @@ public class HelloController {
 		System.out.println("main");
 		
 		return "main";
+	}
+	
+	@RequestMapping(value="/logout" , method = RequestMethod.GET)
+	public String logout(HttpServletRequest request , HttpServletResponse response) {
+		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+		
+		if(auth != null) {
+			new SecurityContextLogoutHandler().logout(request, response, auth);
+		}
+		return "redirect:/login";
 	}
 	
 }
--- webapp/WEB-INF/views/main.jsp
+++ webapp/WEB-INF/views/main.jsp
@@ -1,5 +1,8 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+ <c:set var="root" value="#{pageContext.request.contextPath }"/>
+ <c:set var="logoutUrl" value="/logout"/>
 <!DOCTYPE html>
 <html>
 <head>
@@ -8,5 +11,6 @@
 </head>
 <body>
 	main page
+	<a href="${root}${logoutUrl}">logout</a>
 </body>
 </html>
