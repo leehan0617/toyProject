5ab3145cfecce5249b2bf4a0acab1f648e86d84f
Lee Han Bin <leehan0617@naver.com>
2017-03-08 16:09:51 +0900

    클래스 파일 및 xml 분리

--- src/main/java/com/toy/helloController/HelloController.java
+++ src/main/java/com/toy/helloController/HelloController.java
@@ -3,14 +3,9 @@ package com.toy.helloController;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
@@ -28,43 +23,5 @@ public class HelloController {
 		System.out.println("test");
 		model.addAttribute("name" , name);
 		return "helloWorld";
-	}
-	
-	@RequestMapping("/login")
-	public String login(Model model) {
-		System.out.println("login");
-		
-		return "login";
-	}
-	
-	@RequestMapping("/main")
-	public String main(Model model) {
-		model.addAttribute("user" , getPrincipal());
-		System.out.println("main");
-		
-		return "main";
-	}
-	
-	@RequestMapping(value="/logout" , method = RequestMethod.GET)
-	public String logout(HttpServletRequest request , HttpServletResponse response) {
-		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-		
-		if(auth != null) {
-			new SecurityContextLogoutHandler().logout(request, response, auth);
-		}
-		return "redirect:/login?logout";
-	}
-	
-	private String getPrincipal() {
-		String userName = null;
-		Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-		
-		if (principal instanceof UserDetails) {
-			userName = ((UserDetails)principal).getUsername();
-		} else {
-			userName = principal.toString();
-		}
-		
-		return userName;
 	}
 }
