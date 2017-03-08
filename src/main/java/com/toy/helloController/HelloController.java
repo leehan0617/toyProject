197a2d6efb2f28b342b4c1d153852875075dcbb2
Lee Han Bin <leehan0617@naver.com>
2017-03-08 14:50:34 +0900

    spring security taglib 적용

--- src/main/java/com/toy/helloController/HelloController.java
+++ src/main/java/com/toy/helloController/HelloController.java
@@ -5,6 +5,7 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -38,6 +39,7 @@ public class HelloController {
 	
 	@RequestMapping("/main")
 	public String main(Model model) {
+		model.addAttribute("user" , getPrincipal());
 		System.out.println("main");
 		
 		return "main";
@@ -53,4 +55,16 @@ public class HelloController {
 		return "redirect:/login";
 	}
 	
+	private String getPrincipal() {
+		String userName = null;
+		Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+		
+		if (principal instanceof UserDetails) {
+			userName = ((UserDetails)principal).getUsername();
+		} else {
+			userName = principal.toString();
+		}
+		
+		return userName;
+	}
 }
--- webapp/WEB-INF/views/main.jsp
+++ webapp/WEB-INF/views/main.jsp
@@ -1,6 +1,7 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
 <%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<%@ taglib prefix="sec" uri="http://www.springframework.org/security/tags"%>
  <c:set var="root" value="#{pageContext.request.contextPath }"/>
  <c:set var="logoutUrl" value="/logout"/>
 <!DOCTYPE html>
@@ -12,5 +13,20 @@
 <body>
 	main page
 	<a href="${root}${logoutUrl}">logout</a>
+	<br/>
+	<sec:authorize access="hasRole('ROLE_ADMIN')">
+		당신은 ADMIN 권한 입니다.
+	</sec:authorize>
+	<sec:authorize access="hasRole('ROLE_USER')">
+		당신은 USER 권한 입니다.
+	</sec:authorize>
+	<br/>
+	<sec:authorize access="hasRole('ROLE_ADMIN') or hasRole('ROLE_USER')">
+		여기는 ADMIN 또는 USER 권한 영역입니다.
+	</sec:authorize>
+	
+	<div>
+	${user}
+	</div>
 </body>
 </html>
