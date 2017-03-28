19d5e7d0ddf1271525c8840cbceab139b91aaaea
Lee Han Bin <leehan0617@naver.com>
2017-03-28 10:18:28 +0900

    패키지 통합 및 삭제

--- src/main/java/com/toy/common/controller/ErrorController.java
+++ src/main/java/com/toy/common/controller/ErrorController.java
@@ -1,5 +1,10 @@
 package com.toy.common.controller;
 
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.WebAttributes;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,5 +18,18 @@ public class ErrorController {
 	 public String error(@PathVariable String error,Model model) {
 		 model.addAttribute("error", error);
 		  return "/common/error/error";
+	 }
+	 
+	 /**
+	  * 작성일 : 2017. 3. 28.
+	  * 작성자 : 송하람
+	  * 설  명 : 접근권한이 없을 경우에 이동할 페이지
+	  */
+	 @RequestMapping("/denied")
+	 public String denied(Model model ,  Authentication auth , HttpServletRequest req) {
+		 AccessDeniedException ade = (AccessDeniedException) req.getAttribute(WebAttributes.ACCESS_DENIED_403);
+        model.addAttribute("auth", auth);
+        model.addAttribute("errMsg", ade);
+        return "error/denied";
 	 }
 }
