e0379fe8e1a9bd47240b2b778b9007fff0acc9fe
haram-song <thdgkfka098@github.com>
2017-05-15 19:50:52 +0900

    tiles 응용할수있도록 수정

--- src/main/java/com/toy/issue/controller/IssueController.java
+++ src/main/java/com/toy/issue/controller/IssueController.java
@@ -1,10 +1,21 @@
 package com.toy.issue.controller;
 
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
 /**
  * 작성일 : 2017. 3. 27.
  * 작성자 : 이한빈
  * 설  명 : Issue에 관한 Controller
  */
+@Controller
 public class IssueController {
-
+	
+	@RequestMapping(value={"/issue/list"} , method=RequestMethod.GET)
+	public String showIssueList(HttpServletRequest request) {
+		return "issue/issueList";
+	}
 }
