18f79d1d593c835dc6be7c4a61338139c581ccff
Goodminji <tksxk0@gmail.com>
2017-05-18 09:50:52 +0900

    프로젝트 시퀀스 자동생성 바꾸기 쿼리 및 상태공동코드

--- src/main/resources/sql/settingTable.sql
+++ src/main/resources/sql/settingTable.sql
@@ -11,3 +11,14 @@ insert into department (depart_code , depart_name)
 	values ('art' , 'design');
 insert into department (depart_code , depart_name)
 	values ('sal' , 'sales');
+	
+-- 프로젝트 시퀀스 자동 생성	
+alter table project modify project_id int not null auto_increment;
+
+-- 상태코드
+insert into state (state_code , state_name)
+	values ('0101' , '모집중');
+insert into state (state_code , state_name)
+	values ('0102' , '모집완료');
+insert into state (state_code , state_name)
+	values ('0103' , '신청완료');
