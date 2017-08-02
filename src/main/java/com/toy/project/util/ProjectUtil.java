16a3adc5c23d3c27603bcb6e1d1ec5cf1198cb27
Goodminji <tksxk0@gmail.com>
2017-08-02 13:31:13 +0900

    소나큐브 적용 (14)

---
+++ src/main/java/com/toy/project/util/ProjectUtil.java
@@ -0,0 +1,13 @@
+package com.toy.project.util;
+
+public class ProjectUtil {
+
+	public static final String RECRUIT = "recruit"; //모집상태 가져오기
+	public static final String PROJECT = "project"; //프로젝트상태 가져오기
+	public static final String APPLY = "apply";//승인/수락 code 가져오기
+	public static final String ACCEPT = "accept";//매니저 자동으로 프로젝트 참여 accept:수락 상태 넣기
+	public static final String RECRUITING = "recruiting";//모집시작 상태
+	public static final String START = "start";//프로젝트시작 상태
+	public static final String BATCH = "BATCH";//배치
+	
+}
